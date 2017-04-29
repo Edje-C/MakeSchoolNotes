23ed9fb84ba18119275d39a26505f7f4665a312a
Edje-C <elekson9@yahoo.com>
2017-04-29 15:24:37 -0400

    Make School Notes completed project.

---
+++ MakeSchoolNotes/Controllers/DisplayNoteViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DisplayNoteViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class DisplayNoteViewController: UIViewController {
+    
+    var note:Note?
+    
+    @IBOutlet weak var noteTitleTextField: UITextField!
+    @IBOutlet weak var noteContentTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender:Any?){
+        if segue.identifier == "Save"{
+            let note = self.note ?? Note()
+            note.title = noteTitleTextField.text ?? ""
+            note.content = noteContentTextView.text ?? ""
+            note.modificationTime = Date()
+            CoreDataHelper.saveNote()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let note = note{
+            noteTitleTextField.text = note.title
+            noteContentTextView.text = note.content
+        }else{
+            noteTitleTextField.text = ""
+            noteContentTextView.text = ""
+        }
+    }
+    
+}
---
+++ MakeSchoolNotes/Helpers/CoreDataHelper.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataHelper.swift
+//  MakeSchoolNotes
+//
+//  Created by Elon Jefferson on 4/22/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class CoreDataHelper {
+    static let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    static let persistentContainer = appDelegate.persistentContainer
+    static let managedContext = persistentContainer.viewContext
+    
+    static func saveNote() {
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save \(error)")
+        }
+    }
+    
+    static func delete(note: Note) {
+        managedContext.delete(note)
+        saveNote()
+    }
+    
+    static func retrieveNotes() -> [Note] {
+        let fetchRequest = NSFetchRequest<Note>(entityName: "Note")
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            return results
+        } catch let error as NSError {
+            print("Could not fetch \(error)")
+        }
+        return []
+    }
+}
